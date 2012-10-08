b263d4cf590663cd83e23c27ef5b25261a25690c
bmael <maelbar44@gmail.com>
2012-10-08 19:43:40 +0200

    Add new package in utilities for import csv file

---
+++ src/java/utilities/dataBaseTools/Parser.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilities.dataBaseTools;
+
+import au.com.bytecode.opencsv.CSVReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utilities.fileTools.Download;
+import utilities.fileTools.Zipette;
+
+/**
+ *
+ * @author mael
+ */
+public class Parser {
+    
+    /**
+     * Extract data from the zip file at the url given in parameter
+     * @param url
+     * @return List of data
+     */
+    public static List<String[]> extractData(String url) throws 
+            FileNotFoundException, IOException, MalformedURLException{
+        
+        Download.getFile(url);
+          
+        //Retrieve the name of the zip file and of the csv file
+        String archiveName = url.substring(url.lastIndexOf("/")+1);
+        
+        String[] csvTmpName = archiveName.split("_");
+        String csvName = csvTmpName[0] + "_" + csvTmpName[1] + ".csv";
+                
+        Zipette.extractTo(archiveName, csvName, System.getProperty("user.dir" )+"/");
+            
+        CSVReader readerPmvs = new CSVReader(new FileReader(csvName),';',' ');
+        List<String[] > data = new ArrayList<String[] >();
+
+        String[] nextLine = readerPmvs.readNext();
+        while ((nextLine = readerPmvs.readNext()) != null) {
+            int size = nextLine.length;
+
+            // empty line
+            if (size == 0) continue;
+            
+            String start = nextLine[0].trim();
+            if (start.length() == 0 && size == 1) continue;
+
+            // comment line
+            if (start.startsWith("#")) continue;
+
+            data.add(nextLine);
+        }
+                
+        return data;
+    }
+    
+    public static void main(String args[]) throws 
+            FileNotFoundException, MalformedURLException, IOException{
+        Parser.extractData("http://data.nantes.fr/fileadmin/data/datastore/3-publication/mobilite/localisation_pmv/localisation_pmv_csv.zip");
+    }
+    
+}
